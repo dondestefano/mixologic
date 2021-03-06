d6261bbd7ec27e890a5972bde0a6e351af652151
dondestefano <dondestefano@users.noreply.github.com>
2021-03-06 20:56:07 +0100

    Adds layout for create drink fragment.

--- app/src/main/java/com/example/mixologic/data/Recipe.kt
+++ app/src/main/java/com/example/mixologic/data/Recipe.kt
@@ -2,14 +2,9 @@ package com.example.mixologic.data
 
 data class Recipe (
         val name: String?,
-        val preparation: List<PreparationStep>,
-        val liquors: List<Liquor>,
-        val ingredients: List<Ingredient>
-)
-
-data class PreparationStep(
-        val instruction: String?,
-        val step: Int?,
+        val preparation: String?,
+        val liquors: List<Liquor>?,
+        val ingredients: List<Ingredient>?
 )
 
 data class Ingredient(
